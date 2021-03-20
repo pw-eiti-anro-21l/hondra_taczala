974c78a32f672bb6ee721c696342b70e14336b75
Piotr Hondra <piotrhondra@gmail.com>
2021-03-20 11:01:32 +0100

    minor changes

--- turtle_maneuvering/turtle_maneuvering/maneuver.py
+++ turtle_maneuvering/turtle_maneuvering/maneuver.py
@@ -5,22 +5,21 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 
-class MinimalPublisher(Node):
+class VelocityPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__('velocity_publisher')
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         msg = Twist()
-        msg.linear.x = 2.0
+        msg.linear.x = 0.0
+        msg.angular.z = -2.0
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: linear[{msg.linear.x}, \
-                               {msg.linear.y}, {msg.linear.z}], \
-                                angular[{msg.angular.x}, {msg.angular.y}, \
-                                {msg.angular.z}]')
+        self.get_logger().info(f'Publishing lin.x:{msg.linear.x}, \
+                               ang.z:{msg.angular.z}')
 
     # def timer_callback(self):
     #     msg = String()
@@ -33,14 +32,14 @@ class MinimalPublisher(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    velocity_publisher = VelocityPublisher()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(velocity_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    velocity_publisher.destroy_node()
     rclpy.shutdown()
 
 
