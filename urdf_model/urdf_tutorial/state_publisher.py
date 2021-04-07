1202b5ad73bc367066a3266fdd57ec23cd4f21c5
MichalTaczala <michtaczala@gmail.com>
2021-04-07 12:21:17 +0200

    dodanie plików urdf z robotem

---
+++ urdf_model/urdf_tutorial/state_publisher.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+from math import sin, cos, pi
+import threading
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import JointState
+from tf2_ros import TransformBroadcaster, TransformStamped
+import csv
+class StatePublisher(Node):
+
+  def __init__(self):
+    rclpy.init()
+    super().__init__('state_publisher')
+
+    qos_profile = QoSProfile(depth=10)
+    self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
+    self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
+    self.nodeName = self.get_name()
+    self.get_logger().info("{0} started".format(self.nodeName))
+
+    degree = pi / 180.0
+    loop_rate = self.create_rate(30)
+
+    # robot state
+    self.theta_1 = 0.15
+    self.theta_2 = 10.30
+    self.theta_3 = 20.60
+    # message declarations
+    odom_trans = TransformStamped()
+    odom_trans.header.frame_id = 'odom'
+    odom_trans.child_frame_id = 'part1'
+    joint_state = JointState()
+
+    try:
+      while rclpy.ok():
+          rclpy.spin_once(self)
+
+          # update joint_state
+          now = self.get_clock().now()
+          joint_state.header.stamp = now.to_msg()
+          joint_state.name = ['joint_12', 'joint_23', 'joint_34']
+          joint_state.position = [self.theta_1, self.theta_2, self.theta_3]
+
+          # update transform
+          # (moving in a circle with radius=2)
+          odom_trans.header.stamp = now.to_msg()
+          
+          odom_trans.transform.translation.x = 0.0
+          odom_trans.transform.translation.y = 0.0
+          odom_trans.transform.translation.z = 0.0
+          odom_trans.transform.rotation = \
+            euler_to_quaternion(0, 0, self.theta_1 + self.theta_2/2) # roll,pitch,yaw
+
+
+          self.joint_pub.publish(joint_state)
+          self.broadcaster.sendTransform(odom_trans)
+
+          loop_rate.sleep()
+    except KeyboardInterrupt:
+      pass
+
+def euler_to_quaternion(roll, pitch, yaw):
+  qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+  qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+  qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+  qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+  return Quaternion(x=qx, y=qy, z=qz, w=qw)
+
+def main():
+  node = StatePublisher()
+
+if __name__ == '__main__':
+  main()
