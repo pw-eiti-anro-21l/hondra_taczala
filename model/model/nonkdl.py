375e46699cbc04a74926302bd2bf89ff45991d4c
Piotr Hondra <piotrhondra@gmail.com>
2021-04-24 11:14:35 +0200

    changed viusal matters

--- model/model/nonkdl.py
+++ model/model/nonkdl.py
@@ -5,18 +5,19 @@ from geometry_msgs.msg import Quaternion, PoseStamped
 from rclpy.qos import QoSProfile
 
 import numpy as np
-from math import sin, cos, pi
+from math import sin, cos
 from scipy.spatial.transform import Rotation as R
+
 
 class nonKDL_D(Node):
     def __init__(self):
-            super().__init__('nonKDL_D')
-            self.subscription = self.create_subscription(
-                JointState,
-                'joint_states',
-                self.listener_callback,
-                10)
-            self.subscription  # prevent unused variable warning
+        super().__init__('nonKDL_D')
+        self.subscription = self.create_subscription(
+            JointState,
+            'joint_states',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
     def rotX(self, alpha) -> np.array:
         return np.array([[1, 0, 0, 0],
@@ -47,17 +48,6 @@ class nonKDL_D(Node):
                          ])
 
     def get_item_position(self) -> np.array:
- 
-        # a = 3
-        # theta_z_p = 0
-        # frame1 = PyKDL.Frame(PyKDL.Rotation.RPY(0,0,0), PyKDL.Vector(0, 0, self.d+0.2))
-        # frame2 = PyKDL.Frame(PyKDL.Rotation.RPY(0,0,self.theta1), PyKDL.Vector(0, 0, 0))
-        # frame3 = PyKDL.Frame(PyKDL.Rotation.RPY(0,0,self.theta2), PyKDL.Vector(a, 0, 0))
-        # tool = PyKDL.Frame(PyKDL.Rotation.RPY(0,0,0), PyKDL.Vector(0.5, 0, 0))
-
-        # allFramesInOne = frame1*frame2*frame3*tool
-
-        # return allFramesInOne
         a = 3
         dh = [[0, self.d, 0, 0],
               [0, 0, 0, self.theta1],
@@ -79,29 +69,29 @@ class nonKDL_D(Node):
             transformation = transformation @ frame
         return transformation
 
-
     def listener_callback(self, msg):
         self.d = msg.position[0]
         self.theta1 = msg.position[1]
         self.theta2 = msg.position[2]
-        
+
         itemPosition = self.get_item_position()
         xyz = itemPosition[:3, 3]
         quat = R.from_matrix(itemPosition[:3, :3]).as_quat()
 
-        pose_publisher = self.create_publisher(PoseStamped, '/pose_stamped_KDL_version', QoSProfile(depth=10))
+        pose_publisher = self.create_publisher(
+            PoseStamped, '/pose_stamped_KDL_version', QoSProfile(depth=10))
         pose = PoseStamped()
         pose.header.frame_id = "base_link"
 
-        #na podstawie dokumentacji PoseStamped:
+        # na podstawie dokumentacji PoseStamped:
         pose.pose.position.x = xyz[0]
         pose.pose.position.y = xyz[1]
         pose.pose.position.z = xyz[2]
         # pose.pose.orientation = Quaternion(x=0.0,y=0.0,z=0.0,w=0.0)
-        pose.pose.orientation = Quaternion(x=quat[0], y=quat[1], z=quat[2], w=quat[3])
+        pose.pose.orientation = Quaternion(
+            x=quat[0], y=quat[1], z=quat[2], w=quat[3])
 
         pose_publisher.publish(pose)
-
 
 
 def main(args=None):
@@ -111,5 +101,6 @@ def main(args=None):
     nonKDL_DKIN.destroy_node()
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
-	main()
+    main()
