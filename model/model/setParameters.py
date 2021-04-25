7768d06255907e9724ab5bf5d77a588b294a2a49
MichalTaczala <michtaczala@gmail.com>
2021-04-25 13:13:53 +0200

    naprawienie błędów dla zmieniania wartości D w pliku

--- model/model/setParameters.py
+++ model/model/setParameters.py
@@ -3,26 +3,6 @@ import yaml
 import os
 
 
-# def readYAMLfile():
-# 	with open("model/urdf/model_links_params.yaml", 'r') as file:
-
-# 		data = yaml.load(file, Loader=yaml.FullLoader)
-
-# 	params=[]
-# 	boxX = data['box_x']
-# 	boxY = data['box_y']
-# 	boxZ = data['box_z']
-# 	cylinder_radious = data['cylinder_radious']
-# 	translator_z = data['translator_z']
-# 	pi = data['pi']
-# 	tool_x = data['tool_x']
-# 	tool_y = data['tool_y']
-# 	tool_z = data['tool_z']
-# 	link_offset = data['link_offset']
-
-# 	params.extend((boxX, boxY,boxZ, cylinder_radious, translator_z,pi,tool_x, tool_y, tool_z,link_offset))
-
-# 	return params
 
 def writeYamlFile():
     with open('urdf/model_link_params.yaml', 'r') as file:
@@ -47,9 +27,10 @@ def writeYamlFile():
 		boxY = float(data['box_y']),
 		boxZ = float(data['box_z']),
 		half_box_height = float(data['box_z'])/2,
-		first_link_length = float(data['d']/2+float(data['box_z'])),
-		first_link_z = float(float(data['d']/2+float(data['box_z']))/2+float(data['box_z'])),
+		first_link_length = float(data['d']+2*float(data['cylinder_radius'])),
+		first_link_z = float(data['d']/2+data['box_z']+float(data['cylinder_radius'])),
 		cylinder_radius = float(data['cylinder_radius']),
+
 		pi = float(data['pi']),
 		tool_x = float(data['tool_x']),
 		tool_y = float(data['tool_y']),
